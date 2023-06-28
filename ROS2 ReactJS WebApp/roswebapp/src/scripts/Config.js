6a46ae00655bfa710e8287318fca4298287fa2c0
Kishan Billava <Kishanbillava73@gmail.com>
2023-06-28 14:00:50 +0530

    Update Config.js

--- ROS2 ReactJS WebApp/roswebapp/src/scripts/Config.js
+++ ROS2 ReactJS WebApp/roswebapp/src/scripts/Config.js
@@ -1,6 +1,6 @@
 
 const Config = {
-    ROSBRIDGE_SERVER_IP: "192.168.0.145",
+    ROSBRIDGE_SERVER_IP: "Your IP Address",
     ROSBRIDGE_SERVER_PORT: "9090",
     RECONNECTION_TIMER: 5000,
     CMD_VEL_TOPIC:"/cmd_vel", // /turtle1/cmd_vel
